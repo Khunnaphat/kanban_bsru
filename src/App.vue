cc8336f222d405437dd948f09d35bf05fa39ac4c
champ <counterchamp002@gmail.com>
2020-12-03 17:41:35 +0700

    777

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,71 @@
 <template>
   <div id="app">
-    <Kanban :data="data" :create_task_submit="create_task_submit"/>
+    <Kanban
+      :data="data"
+      :create_task_submit="create_task_submit"
+      :move_item_task="move_item_task"
+    />
   </div>
 </template>
 
 <script>
-import Kanban from "./components/Kanban"
+import Kanban from "./components/Kanban";
+
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Kanban
+    Kanban,
   },
-  data(){
+  data() {
     return {
-      data:[
+      data: [
         {
-          name:"todo",
-          color:"#F5B271",
-          tasks:[
+          name: "todo",
+          color: "#84FBF7",
+          tasks: [
             {
-              task_name:"tesk"
-            }
-          ]
+              task_name: "test",
+            },
+          ],
         },
-         {
-          name:"doing",
-          color:"#BEA771",
-          tasks:[
-                       {
-              task_name:"tesk"
-            }
-          ]
+        {
+          name: "doing",
+          color: "#C0FB84",
+          tasks: [
+            {
+              task_name: "test",
+            },
+          ],
         },
-         {
-          name:"done",
-          color:"#1B796F",
-          tasks:[
-                       {
-              task_name:"tesk"
-            }
-          ]
-        }
-      ]
-    }
+        {
+          name: "done",
+          color: "#F69D9D",
+          tasks: [
+            {
+              task_name: "test",
+            },
+          ],
+        },
+      ],
+    };
   },
-  methods:{
-    create_task_submit(colum_index,item){
-      this.data[colum_index].tasks.push(item)
-    }
-  }
-}
+  methods: {
+    create_task_submit(column_index, item) {
+      this.data[column_index].tasks.push(item);
+    },
+    move_item_task(column_a, index_task_a, column_b, index_task_b) {
+      var item = this.data[column_a].tasks[index_task_a];
+
+      this.data[column_a].tasks.splice(index_task_a, 1);
+
+      this.data[column_b].tasks.splice(index_task_b, 0,item);
+    },
+  },
+};
 </script>
 
 <style>
-  #app {
+#app {
   height: 100vh;
-  }
+}
 </style>
